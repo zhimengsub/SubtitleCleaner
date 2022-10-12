f1a943c10f10e30569b5d042aa221ac443e9555d
barryZZJ <850462618@qq.com>
2022-10-12 19:35:47 +0800

    v1.0

---
+++ FullwidthConverter.py
@@ -0,0 +1,100 @@
+import argparse
+import os
+import traceback
+from argparse import RawTextHelpFormatter
+
+VER = 'v1.0'
+
+DESCRIPTION = '全角片假名转换器\n' + \
+              '本程序可以将半角片假名/符号转换为全角形式\n\n' + \
+              '使用方法：将待转换文件拖放到本程序上即可，也可以使用命令行运行进行更多配置。\n' + \
+              '—— ' + VER + ' by 谢耳朵w\n' + \
+              '最新版本请前往 https://github.com/barryZZJ/SubtitleCleaner 获取'
+
+lookup = {
+    '｡': '。', '｢': '「', '｣': '」', '､': '、', '･': '・',
+    'ｶﾞ': 'ガ', 'ｷﾞ': 'ギ', 'ｸﾞ': 'グ', 'ｹﾞ': 'ゲ', 'ｺﾞ': 'ゴ',
+    'ｻﾞ': 'ザ', 'ｼﾞ': 'ジ', 'ｽﾞ': 'ズ', 'ｾﾞ': 'ゼ', 'ｿﾞ': 'ゾ',
+    'ﾀﾞ': 'ダ', 'ﾁﾞ': 'ヂ', 'ﾂﾞ': 'ヅ', 'ﾃﾞ': 'デ', 'ﾄﾞ': 'ド',
+    'ﾊﾞ': 'バ', 'ﾋﾞ': 'ビ', 'ﾌﾞ': 'ブ', 'ﾍﾞ': 'ベ', 'ﾎﾞ': 'ボ',
+    'ﾊﾟ': 'パ', 'ﾋﾟ': 'ピ', 'ﾌﾟ': 'プ', 'ﾍﾟ': 'ペ', 'ﾎﾟ': 'ポ',
+    'ｧ': 'ァ', 'ｨ': 'ィ', 'ｩ': 'ゥ', 'ｪ': 'ェ', 'ｫ': 'ォ',
+    'ｬ': 'ャ', 'ｭ': 'ュ', 'ｮ': 'ョ', 'ｯ': 'ッ', 'ｰ': 'ー',
+    'ｱ': 'ア', 'ｲ': 'イ', 'ｳ': 'ウ', 'ｴ': 'エ', 'ｵ': 'オ',
+    'ｶ': 'カ', 'ｷ': 'キ', 'ｸ': 'ク', 'ｹ': 'ケ', 'ｺ': 'コ',
+    'ｻ': 'サ', 'ｼ': 'シ', 'ｽ': 'ス', 'ｾ': 'セ', 'ｿ': 'ソ',
+    'ﾀ': 'タ', 'ﾁ': 'チ', 'ﾂ': 'ツ', 'ﾃ': 'テ', 'ﾄ': 'ト',
+    'ﾅ': 'ナ', 'ﾆ': 'ニ', 'ﾇ': 'ヌ', 'ﾈ': 'ネ', 'ﾉ': 'ノ',
+    'ﾊ': 'ハ', 'ﾋ': 'ヒ', 'ﾌ': 'フ', 'ﾍ': 'ヘ', 'ﾎ': 'ホ',
+    'ﾏ': 'マ', 'ﾐ': 'ミ', 'ﾑ': 'ム', 'ﾒ': 'メ', 'ﾓ': 'モ',
+    'ﾔ': 'ヤ', 'ﾕ': 'ユ', 'ﾖ': 'ヨ',
+    'ﾗ': 'ラ', 'ﾘ': 'リ', 'ﾙ': 'ル', 'ﾚ': 'レ', 'ﾛ': 'ロ',
+    'ﾜ': 'ワ', 'ﾝ': 'ン', 'ｦ': 'ヲ',
+}
+
+def initparser():
+    parser = argparse.ArgumentParser(description=DESCRIPTION, formatter_class=RawTextHelpFormatter)
+    parser.add_argument('InputFile', type=str, help='待转换文本文件的路径，仅支持utf-8、GBK编码。')
+    parser.add_argument('-o', '--output', metavar='FILE', type=str, help='输出文件名，默认为<输入文件名>_out.txt。')
+    return parser
+
+def mkOutfilename(infile: str):
+    name, suf = os.path.splitext(infile)
+    return name+'_out'+suf
+
+def convertline(line: str, lookup: dict):
+    # 日字的数字、全角空格、全角标点符号不能改，可能还是改回查找表，并且额外增加浊音半浊音
+    # line = unicodedata.normalize('NFKC', line)
+    for old, new in lookup.items():
+        line = line.replace(old, new)
+    return line
+
+def doconvert(inname, outname, lookup):
+    cnter = 0
+    encodings = ['utf-8-sig', 'gbk']
+    infile = None
+    outfile = None
+    for encoding in encodings:
+        try:
+            infile = open(inname, 'r', encoding=encoding)
+            outfile = open(outname, 'w', encoding=encoding)
+            while line := infile.readline():
+                nline = convertline(line, lookup)
+                if nline != line:
+                    cnter += 1
+                    print(line.rstrip('\n'), '->\n\t', nline.rstrip('\n'))
+                    print()
+                outfile.write(nline)
+            print('\n完成! 共转换了', cnter, '行，已保存至', outname)
+            return True
+        except UnicodeDecodeError:
+            continue
+        except Exception as err:
+            print('\n发生了未知错误！请将下面的报错信息及待转换文件提交到 https://github.com/barryZZJ/SubtitleCleaner/issues\n')
+            traceback.print_exc()
+            return False
+        finally:
+            if infile:
+                infile.close()
+            if outfile:
+                outfile.close()
+
+    print('\n错误！无法识别的文件编码，请将文件转存为UTF8或GBK编码格式再试！')
+    return False
+
+def main():
+    parser = initparser()
+    args = parser.parse_args()
+    print(DESCRIPTION)
+    print()
+    print('正在读取', args.InputFile)
+
+    outname = args.output or mkOutfilename(args.InputFile)
+    doconvert(args.InputFile, outname, lookup)
+
+    print()
+    os.system('pause')
+    #input('\nPress return to continue...')
+
+if __name__ == '__main__':
+    main()
---
+++ SubCleaner.py
@@ -0,0 +1,161 @@
+import argparse
+import os
+from argparse import RawTextHelpFormatter
+import re
+import traceback
+import ass
+from ass import Dialogue
+from ass_tag_parser import parse_ass, AssText
+from FullwidthConverter import convertline, lookup
+
+VER = 'v1.0'
+
+DESCRIPTION = '字幕清理器\n' + \
+              '把.ass文件中的对话部分提取出来，并进行半角片假名转换、多余符号清洗等各种操作\n\n' + \
+              '使用方法：将待转换文件拖放到本程序上即可，也可以使用命令行运行进行更多配置。\n' + \
+              '—— ' + VER + ' by 谢耳朵w\n' + \
+              '详细介绍及最新版本请前往 https://github.com/barryZZJ/SubtitleCleaner 获取'
+
+pats = [
+    # remove …
+    (re.compile(r'…'), ''),
+    # remove 。(full width)
+    (re.compile(r'。'), ''),
+    # remove ｡
+    (re.compile(r'｡'), ''),
+    # remove ！
+    (re.compile(r'！'), ''),
+    # 顿号、改为半角空格
+    (re.compile(r'、'), ' '),
+    # remove (...)
+    (re.compile(r'\(.*\)'), ''),
+    # remove [...]
+    (re.compile(r'\[.*\]'), ''),
+    # remove \N
+    (re.compile(r'\\N'), ''),
+    # TODO \)\n替换成) ?
+]  # type: list[tuple[re.Pattern, str]]
+
+# add \N at each line's start
+pats_final = [
+    (re.compile(r'^'), r'\\N')
+]  # type: list[tuple[re.Pattern, str]]
+
+def initparser():
+    parser = argparse.ArgumentParser(description=DESCRIPTION, formatter_class=RawTextHelpFormatter)
+    parser.add_argument('InputFile', type=str, help='待转换ass文件的路径。')
+    parser.add_argument('-o', '--output', metavar='FILE', type=str, help='输出文件名，默认为<输入文件名>_out.txt。')
+    return parser
+
+def mkOutfilename(infile: str):
+    name, suf = os.path.splitext(infile)
+    return name+'_out.txt'
+
+def shouldMerge(event1:Dialogue, event2:Dialogue):
+    if event1.text.endswith(r'…\N'):
+        return 2
+    if event1.start == event2.start and event1.end == event2.end:
+        return 1
+    return -1
+
+def cleanline(line:str, pats:list[tuple[re.Pattern, str]]):
+    for pat, repl in pats:
+        line = pat.sub(repl, line)
+    return line
+
+def removeSFX(line:str):
+    arr = parse_ass(line)
+    texts = [a.text for a in arr if isinstance(a, AssText)]
+    line = ''.join(texts)
+    return line
+
+def doclean(inname, outname, pats, lookup):
+    encoding = 'utf-8-sig'
+    outfile = None
+    try:
+        with open(inname, encoding=encoding) as f:
+            doc = ass.parse(f)
+        outfile = open(outname, 'w', encoding='utf-8')
+
+        # merge lines by time and '…\N'
+        cnter = 0
+        j = 0
+        i = 0
+        mergedTexts = []
+        print('合并台词中...\n')
+        while i < len(doc.events):
+            if doc.events[i].TYPE != 'Dialogue':
+                i += 1
+                continue
+            otext = ntext = removeSFX(doc.events[i].text)
+            # find events that should merge together
+            j = i
+            reasons = []
+            while j+1 < len(doc.events) and (res:=shouldMerge(doc.events[j], doc.events[j+1])) > 0:
+                cnter += 1
+                if res == 1:
+                    # same time, merge with half-width space
+                    ntext = ntext + ' ' + removeSFX(doc.events[j+1].text)
+                    reason = '(时间相同)'
+                elif res == 2:
+                    # ends with …\N, merge with full-width space
+                    ntext = ntext + '　' + removeSFX(doc.events[j+1].text)
+                    reason = r'(以…\N结尾)'
+                else:
+                    raise NotImplementedError('Merge result not implemented!')
+                j += 1
+                reasons.append(reason)
+            if j != i:
+                print(*reasons, otext, '->\n\t', ntext)
+                print()
+            mergedTexts.append(ntext)
+            i = j + 1
+        print('合并完毕！新并入了', cnter, '行对白。\n')
+
+        # process line by line
+        print('清理台词中...')
+        cnter = 0
+        for line in mergedTexts:
+            cnter += 1
+            # remove sfx codes
+            # remove redundancies in line and convert halfwidth katakana
+            nline = cleanline(line, pats)
+            nline = convertline(nline, lookup)
+            # 跳过空行
+            if nline == '':
+                print(line, '->')
+                print()
+                continue
+            nline = cleanline(nline, pats_final)
+            print(line, '->\n\t', nline)
+            print()
+            outfile.write(nline)
+            outfile.write('\n')
+        print('\n完成! 共处理了', cnter, '行对白，已保存至', outname)
+        return True
+    except Exception as err:
+        print(
+            '\n发生了未知错误！请将下面的报错信息及待转换文件提交到 https://github.com/barryZZJ/SubtitleCleaner/issues')
+        traceback.print_exc()
+        return False
+    finally:
+        if outfile:
+            outfile.close()
+
+def main():
+    parser = initparser()
+    args = parser.parse_args()
+    print(DESCRIPTION)
+    print()
+    print('正在读取', args.InputFile)
+
+    outname = args.output or mkOutfilename(args.InputFile)
+    doclean(args.InputFile, outname, pats, lookup)
+
+    print()
+    os.system('pause')
+
+
+if __name__ == '__main__':
+    main()
+
