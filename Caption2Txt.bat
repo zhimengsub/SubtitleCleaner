aa292751e35845b8f1aba419562264a3b334e98c
barryZZJ <850462618@qq.com>
2022-10-15 09:18:55 +0800

    fix bugs in caption2txt

--- Caption2Txt.bat
+++ Caption2Txt.bat
@@ -1,14 +1,16 @@
-@ 根据ts生成清理后的台词文本文件。
-@ 使用方法：拖入ts文件，会先生成ass，然后生成处理后的txt文件。
-@ 注意：必须把本脚本与Caption2Ass_PCR.exe、SubCleaner.exe放在同一目录下才能正常工作！
-@ by 谢耳朵w v1.0
+@REM 根据ts生成清理后的台词文本文件。
+@REM 使用方法：拖入ts文件，会先生成ass，然后生成处理后的txt文件。
+@REM 注意：必须把本脚本与Caption2Ass_PCR.exe、SubCleaner.exe放在同一目录下才能正常工作！
+@REM by 谢耳朵w v1.1
 
 @echo off
+cd /d "%~dp0"
 
 Caption2Ass_PCR.exe -norubi "%~1"
 
 echo.
 
-SubCleaner.exe "%~n1.ass" -q
+@REM 添加--log可以把程序的输出保存到文件中
+SubCleaner.exe "%~dpn1.ass" -q
 
 pause
